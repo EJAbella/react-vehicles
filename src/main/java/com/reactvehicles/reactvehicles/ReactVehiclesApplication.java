708537744c6593b20fc039966b6df51a7050e623
Elijah <elijah.abella@aol.com>
2018-09-21 11:52:56 -0700

    first commit

---
+++ src/main/java/com/reactvehicles/reactvehicles/ReactVehiclesApplication.java
@@ -0,0 +1,12 @@
+package com.reactvehicles.reactvehicles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactVehiclesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactVehiclesApplication.class, args);
+    }
+}
---
+++ src/test/java/com/reactvehicles/reactvehicles/ReactVehiclesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.reactvehicles.reactvehicles;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactVehiclesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
